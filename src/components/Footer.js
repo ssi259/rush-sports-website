a250f40131da0c7629d02ddaefb3254a5a998dba
Arjun <arjun.verma8412@gmail.com>
2022-11-14 18:06:00 +0530

    add_policies

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ function Footer(){
         <ul className='nav-ul'>
             <li><Link to = "/about" >About</Link></li>
             <li><Link to = "/contactUs" >Contact Us</Link></li>
-            <li><Link to = "/policies" >Policies</Link></li>
-            <li><Link to = "/policy" >Policy</Link></li>
+            <li><Link to = "/policies" >Cancellation & Refund Policy</Link></li>
+            <li><Link to = "/policy" >Privacy Policy</Link></li>
 
         </ul>
     </div>)
